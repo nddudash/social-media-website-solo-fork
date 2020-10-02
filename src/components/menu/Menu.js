4148d292fb8e90b79c77b0f081bc5aaaa3ca0170
Nick D <nddudash@fouytan.local>
2020-10-02 15:51:24 -0600

    Last Minute Touch-Ups

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -22,7 +22,7 @@ class Menu extends React.Component {
   render() {
     return (
       <div className="Menu">
-        <h1>Kwitter</h1>
+        <Header as = "h1">Kwitter</Header>
         {this.props.isAuthenticated && (
           <div id="menu-links">
             <Link to="/mainHub">
